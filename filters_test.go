7e5bc38d1d3bae1868381ea08f95527daddd086f
Kotone Itaya <kotone@sfc.keio.ac.jp>
2020-01-07 02:15:39 +0900

    Remove dependency to assert and restructure tests

--- filters_test.go
+++ filters_test.go
@@ -1,51 +1,52 @@
-package ascii_test
+package ascii
 
 import (
 	"testing"
 
-	"gopkg.in/ktnyt/ascii.v1"
-	"gopkg.in/ktnyt/assert.v1"
+	assert "gopkg.in/ktnyt/assert.v1"
 )
 
-func filterTest(set []byte, filter ascii.Filter) assert.F {
+func filterTest(set []byte, filter Filter) assert.F {
 	cases := make([]assert.F, 128)
 	for i := range cases {
 		b := byte(i)
-		v, e := filter(b), ascii.Contains(set)(b)
+		v, e := filter(b), Contains(set)(b)
 		cases[i] = assert.Equal(v, e)
 	}
 	return assert.All(cases...)
 }
 
-type testCaseType struct {
-	Name   string
-	Set    []byte
-	Filter ascii.Filter
-}
-
-var testCases = []testCaseType{
-	testCaseType{"Null", ascii.Null, ascii.IsNull},
-	testCaseType{"Graphic", ascii.Graphic, ascii.IsGraphic},
-	testCaseType{"Control", ascii.Control, ascii.IsControl},
-	testCaseType{"Space", ascii.Space, ascii.IsSpace},
-	testCaseType{"Quote", ascii.Quote, ascii.IsQuote},
-	testCaseType{"Upper", ascii.Upper, ascii.IsUpper},
-	testCaseType{"Lower", ascii.Lower, ascii.IsLower},
-	testCaseType{"Letter", ascii.Letter, ascii.IsLetter},
-	testCaseType{"Digit", ascii.Digit, ascii.IsDigit},
-	testCaseType{"Binary", ascii.Binary, ascii.IsBinary},
-	testCaseType{"Octal", ascii.Octal, ascii.IsOctal},
-	testCaseType{"Hex", ascii.Hex, ascii.IsHex},
-	testCaseType{"Latin", ascii.Latin, ascii.IsLatin},
-	testCaseType{"Snake", ascii.Snake, ascii.IsSnake},
-	testCaseType{"ASCII", ascii.ASCII, ascii.IsASCII},
-	testCaseType{"Byte", ascii.Byte, ascii.IsByte},
+var testCases = []struct {
+	name   string
+	set    []byte
+	filter Filter
+}{
+	{"Null", Null, IsNull},
+	{"Graphic", Graphic, IsGraphic},
+	{"Control", Control, IsControl},
+	{"Space", Space, IsSpace},
+	{"Quote", Quote, IsQuote},
+	{"Upper", Upper, IsUpper},
+	{"Lower", Lower, IsLower},
+	{"Letter", Letter, IsLetter},
+	{"Digit", Digit, IsDigit},
+	{"Binary", Binary, IsBinary},
+	{"Octal", Octal, IsOctal},
+	{"Hex", Hex, IsHex},
+	{"Latin", Latin, IsLatin},
+	{"Snake", Snake, IsSnake},
+	{"ASCII", ASCII, IsASCII},
+	{"Byte", Byte, IsByte},
 }
 
 func TestFilters(t *testing.T) {
-	cases := make([]assert.F, len(testCases))
-	for i, testCase := range testCases {
-		cases[i] = assert.C(testCase.Name, filterTest(testCase.Set, testCase.Filter))
+	for _, tt := range testCases {
+		for i := 0; i < 128; i++ {
+			b := byte(i)
+			act, exp := tt.filter(b), Contains(tt.set)(b)
+			if act != exp {
+				t.Errorf("%[1]s(0x%[2]x) != Is%[1]s(0x%[2]x)", tt.name, b)
+			}
+		}
 	}
-	assert.Apply(t, cases...)
 }
